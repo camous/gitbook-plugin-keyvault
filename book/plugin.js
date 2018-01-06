f418fd333444840a1f05aa0d4f210a0cc4f9fffa
camous <camous.fabien@gmail.com>
2018-01-06 22:47:36 +0100

    updated version of plugin

--- book/plugin.js
+++ book/plugin.js
@@ -1,19 +1,71 @@
-require(["gitbook", "jquery"], function (gitbook, $) {
-	console.log('gitbook-plugin-keyvault initiated');
-    // mermaidAPI.initialize({
-        // startOnLoad: false
-    // });
+require(['gitbook', 'jquery'], function (gitbook, $) {
+  console.log('gitbook-plugin-keyvault initiated');
 
-    // gitbook.events.bind("page.change", function () {
-        // $('code.lang-graph').each(function (index, element) {
-            // var $element = $(element);
-            // var code = $element.text();
+  gitbook.events.bind('page.change', function () {
+    $('code.lang-keyvaultsecret').each(function (index, element) {
+      var $element = $(element);
+      var $pre = $element.parent();
+      var code = $element.text().trim();
 
-            // var insertSvg = function (svgCode, bindFunctions) {
-                // $element.html(svgCode);
-            // };
+      var settings = {
+        crossDomain: true,
+        url: '/keyvault/secret/' + encodeURIComponent(code),
+        method: 'GET'
+      };
 
-            // mermaidAPI.render('graphDiv-' + index, code, insertSvg);
-        // });
-    // });
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+        $pre.replaceWith(printSecret(response));
+      });
+    });
+
+    function printSecret(secret) {
+
+      var secretValue = secret.value || (secret.error + ' needs one of ' + secret.tags.roles.replace(/"/g, '\''));
+
+      return `<div class="keyvault">
+        <h6>` + secret.name.toUpperCase() + `</h6>
+        <div>
+          <div class="valuediv">
+            <input id="input_` + secret.name + `" class="value" value="` + secretValue + `" />
+          </div>
+          <div class="actiondiv">
+            <a href="" onclick="copyToClipboard('input_` + secret.name + `');"><i class="fa fa-clipboard" aria-hidden="true"></i></a>&nbsp; 
+            <a href="#"><i class="fa fa-info-circle" aria-hidden="true"></i></a>
+          </div>
+        </div>
+        <div class="detailsdiv" id="details_` + secret.name + `"><pre>
+          ` + JSON.stringify(secret) + `
+        </pre></div>
+      </div>`;
+    }
+
+    $('code.lang-keyvaultnamespace').each(function (index, element) {
+      var $element = $(element);
+      var $pre = $element.parent();
+      var code = $element.text().trim();
+
+      var settings = {
+        crossDomain: true,
+        url: '/keyvault/namespace/' + encodeURIComponent(code),
+        method: 'GET'
+      };
+
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+        var list = "";
+        for (var id in response){
+          var secret = response[id];
+          list += printSecret(secret);
+        }
+        $pre.replaceWith(list);
+      });
+    });
+  });
 });
+
+function copyToClipboard(input) {
+  $('#' + input).focus().select();
+  document.execCommand('copy');
+  console.log('copy to clipboard');
+}
